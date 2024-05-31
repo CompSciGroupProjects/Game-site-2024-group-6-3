4fc3eb833c3eccb7df5bad208be23336f33e6e20
Luksh Mahajan <113115939+LukshMahajan@users.noreply.github.com>
2024-05-31 13:59:33 -0400

    links

---
+++ script.js
@@ -0,0 +1,9 @@
+function enterConnect4(){
+    window.location.href="Connect4Rules";
+}
+function enterTTT(){
+    window.location.href="nicksRulePage";
+}
+function enterRPC(){
+    window.location.href="rpcRules";
+}
